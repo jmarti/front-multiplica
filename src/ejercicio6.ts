fb7b61423052594e9912822d7440a7ce9b8a50ba
Jordi Martí Domínguez <jmarti@MacBook-Pro-de-Jordi.local>
2020-09-08 18:36:03 -0500

    mejoras ejercicio6

--- src/ejercicio6.ts
+++ src/ejercicio6.ts
@@ -1,33 +1,33 @@
 export let arrPersonas = [
-    {
-        name: 'Pepe',
-        donacion: true,
-        esposas: ['Rosangela', 'Mayte']
-    },
-    {
-      name: 'Juan',
-      donacion: false,
-      esposas: ['Yahaira']
-    },
-    {
-      name: 'Lalo',
-      donacion: true,
-      esposas: []
-    }
-  ]
-  
+  {
+    name: 'Pepe',
+    donacion: true,
+    esposas: ['Rosangela', 'Mayte']
+  },
+  {
+    name: 'Juan',
+    donacion: false,
+    esposas: ['Yahaira']
+  },
+  {
+    name: 'Lalo',
+    donacion: true,
+    esposas: []
+  }
+];
+
 
 export const func = (idx: number) => {
     
-    const persona = getPersona(idx);
+  const persona = getPersona(idx);
 
-    if (typeof persona === 'undefined') {
-        return {};
-    }
+  if (typeof persona === 'undefined') {
+      return {};
+  }
 
-    return {...persona, name: 'choy'};
+  return {...persona, name: 'choy'};
 }
 
 export const getPersona = (idx: number) => {
-    return arrPersonas[idx];
+  return arrPersonas[idx];
 }
