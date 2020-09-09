95d1c220d66bc9d6945eadec9a9d230bd9a8e814
Jordi Martí Domínguez <jmarti@MacBook-Pro-de-Jordi.local>
2020-09-09 18:46:02 -0500

    refactor ejercicio10

--- src/ejercicio10.spec.ts
+++ src/ejercicio10.spec.ts
@@ -1,26 +1,99 @@
 import { func } from './ejercicio10';
-
-const arr = [
-    {
-        name: 'Pepe',
-        donacion: true,
-        esposas: ['Rosangela', 'Mayte']
-    },
-    {
-        name: 'Juan',
-        donacion: false,
-        esposas: ['Yahaira']
-    },
-    {
-        name: 'Lalo',
-        donacion: true,
-        esposas: []
-    }
-];
+import { isRegExp } from 'util';
+import { arrPersonas } from './ejercicio6';
 
 describe(`ejercicio 10`, () => {
-    it(`devuelve solo los nombres de las personas que donan órganos y tienen alguna esposa cuyo nombre empiece con 'Y' o 'N'`, () => {
+    it(`quita las personas que no donan organos`, () => {
+        const result = func([
+            {
+                name: 'Juan',
+                donacion: true,
+                esposas: ['Nadia']
+            },
+            {
+                name: 'Lalo',
+                donacion: false,
+                esposas: []
+            }           
+        ]);
+
+        expect(result).toStrictEqual(['Juan']);
+    });
+
+    it(`quita las personas que donan organos pero que no tienen esposas`, () => {
+        const result = func([
+            {
+                name: 'Juan',
+                donacion: true,
+                esposas: ['Yahaira']
+            },
+            {
+                name: 'Lalo',
+                donacion: true,
+                esposas: []
+            }           
+        ]);
+
+        expect(result).toStrictEqual(['Juan']);
+    });
+
+    it(`quita las personas que donan organos pero que no tienen esposas`, () => {
+        const result = func([
+            {
+                name: 'Juan',
+                donacion: true,
+                esposas: ['Nadia']
+            },
+            {
+                name: 'Lalo',
+                donacion: true,
+                esposas: []
+            }           
+        ]);
+
+        expect(result).toStrictEqual(['Juan']);
+    });
+
+    it(`quita las personas que donan organos y que tienen esposas, pero que el nombre de ninguna de sus esposas empieza por 'Y' o por 'N'`, () => {
+        const result = func([
+            {
+                name: 'Juan',
+                donacion: true,
+                esposas: ['Nadia']
+            },
+            {
+                name: 'Lalo',
+                donacion: true,
+                esposas: []
+            }           
+        ]);;
+        expect(result).toStrictEqual(['Juan']);
+    });
+
+    it(`comprueba que dado el array del enunciado, el resultado sea '[]'`, () => {
+        const arr = [
+            {
+              name: 'Pepe',
+              donacion: true,
+              esposas: ['Rosangela', 'Mayte']
+            },
+            {
+              name: 'Juan',
+              donacion: false,
+              esposas: ['Yahaira']
+            },
+            {
+              name: 'Lalo',
+              donacion: true,
+              esposas: []
+            }
+        ];
+
         const result = func(arr);
+
         expect(result).toStrictEqual([]);
     });
 });
+
+
+
--- src/ejercicio10.ts
+++ src/ejercicio10.ts
@@ -5,21 +5,22 @@ interface Persona {
 };
 
 export const func = (personas: Persona[]) => {
-    return personas.reduce((nombres: string[], p) => {
+    return personas.filter((p) => {
+
+        if (!p.donacion) return false;
+
+        if (p.esposas.length === 0) return false;
+
         
-        if (!p.donacion) return nombres;
-
-        if (!(p.esposas.length > 0)) return nombres;
-
         for (let esposa of p.esposas) {
             const inicialEsposa = esposa.charAt(0);
             
             if (inicialEsposa === 'Y' || inicialEsposa === 'N') {
-                return [...nombres, p.name];
+                return true;
             }
         }
 
-        return nombres;
+        return false;
     
-    }, []);
+    }).map((p) => p.name);
 };
