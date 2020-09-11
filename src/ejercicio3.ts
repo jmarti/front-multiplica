0ff26798e02ef065add0794f3dc30f9b4884db93
Jordi Martí Domínguez <jmarti@MacBook-Pro-de-Jordi.local>
2020-09-11 15:31:02 -0500

    ejercicio3

--- src/ejercicio3.ts
+++ src/ejercicio3.ts
@@ -1,24 +1,44 @@
 import { readFileSync } from "fs";
 
+type ConfigRule = (value: any) => any;
+type RuleMapper = Record<string, ConfigRule>;
+type ConfigObject = Record<string, any>;
+
+
+const readConfig = (ruta: string): ConfigObject => {
+
+    const validatePort: ConfigRule = (portNumber: any) => parseInt(portNumber) || 6969;
+    // por simular que se tienen mas reglas para más elementos de la configuracion
+    const validateHost: ConfigRule = (host: any) => '127.0.0.1';
+    const validateUsername: ConfigRule = (username: any) => 'jordi';
+
+    const fieldRules: RuleMapper = {
+        puerto: validatePort,
+        host: validateHost,
+        username: validateUsername,
+    }
+
+    const readConfigFile = (ruta: string): ConfigObject => {
+        try {
+            const config = readFileSync(ruta);
+            return JSON.parse(config.toString());
+        } catch {
+            return {};
+        }
+    }
+
+    const configObject = readConfigFile(ruta);
+
+    const validEntries = Object.entries(fieldRules)
+        .map(([name, validation]) => {
+            return {[name]: validation(configObject[name])};
+        })
+        .filter(e => e);
+
+    return Object.assign({}, ...validEntries);
+};
+
+
 export const readPort = (ruta: string): any => {
-    const DEFAULT_PORT = 6969; 
-    
-    try {
-        const config = readFileSync(ruta);
-        const puerto = JSON.parse(config.toString()).puerto;
-
-        if (typeof puerto === 'undefined') {
-            throw new Error();
-        }
-
-        if (isNaN(puerto)) {
-            throw new Error();
-        }
-
-        return parseInt(puerto);
-    }
-    
-    catch {
-        return DEFAULT_PORT;
-    }
+    return readConfig(ruta).puerto;
 };
