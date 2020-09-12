9d2c443eeee5d2d75ebad4378c58c1d96cb9e210
Jordi Martí Domínguez <jmarti@MacBook-Pro-de-Jordi.local>
2020-09-11 19:33:06 -0500

    jestconfig modification to work with dom

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node',
+    testEnvironment: 'jsdom',
     verbose: true,
     collectCoverage: false,
     collectCoverageFrom: ['src/**/*.ts']
